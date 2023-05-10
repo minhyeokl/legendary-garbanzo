70e7d96daa8388eb766271cbea5f38d3c96cb478
Minhyeok Lee <whereverigotheygo@gmail.com>
2023-05-10 13:40:47 +0900

    Update tokenizer to parse directory recursively

--- tokenizer.py
+++ tokenizer.py
@@ -17,29 +17,25 @@ def save_tokenized_sentences(sentences, output_filename):
             line = sentence.strip() + "\n"
             f.write(line)
 
+def process_files(directory):
+    for filename in os.listdir(directory):
+        file_path = os.path.join(directory, filename)
+        if os.path.isdir(file_path):
+            process_files(file_path)  # 디렉터리인 경우 재귀적으로 처리
+        else:
+            sentences = tokenize_file(file_path)
+            output_filename = os.path.basename(file_path)
+            output_filename = os.path.join(args.output, os.path.splitext(output_filename)[0] + "_tokenized.txt")
+            save_tokenized_sentences(sentences=sentences, output_filename=output_filename)
+
+
 def init():
     parser = argparse.ArgumentParser(description='Tokenize files in a directory or a single file.')
     parser.add_argument('source', metavar='SOURCE', type=str, help='path to the source directory or file')
     parser.add_argument('output', metavar='OUTPUT', type=str, default = "output/" ,help='path to the output directory')
     args = parser.parse_args()
 
-    if os.path.isdir(args.source):
-        # 디렉터리인 경우
-        for filename in os.listdir(args.source):
-            file_path = os.path.join(args.source, filename)
-            if os.path.isfile(file_path):
-                # 파일인 경우 함수 실행
-                sentences = tokenize_file(file_path)
-                output_filename = os.path.basename(file_path)
-                output_filename = os.path.join(args.output, os.path.splitext(output_filename)[0] + "_tokenized.txt")
-                save_tokenized_sentences(sentences=sentences, output_filename=output_filename)
-
-    else:
-        # 파일인 경우 함수 실행
-        sentences = tokenize_file(args.source)
-        output_filename = os.path.basename(file_path)
-        output_filename = os.path.splitext(output_filename)[0] + "_tokenized.txt"
-        save_tokenized_sentences(sentences=sentences, output_filename=output_filename)
-
+    process_files(args.source)
+    
 if __name__ == "__main__":
     init()
