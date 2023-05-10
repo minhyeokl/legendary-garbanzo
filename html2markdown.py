ba381cb4dcd3c50a609c80c67a489f8f42e8a519
Minhyeok Lee <whereverigotheygo@gmail.com>
2023-05-10 17:01:00 +0900

    html2markdown.py

--- html2markdown.py
+++ html2markdown.py
@@ -1,5 +1,17 @@
 import os
 import argparse
+import markdownify
+
+
+def convert_html_to_markdown(input_file, output_dir):
+    output_filename = os.path.basename(input_file)
+    output_filename = os.path.join(output_dir, os.path.splitext(output_filename)[0] + ".md")
+    
+    with open(input_file, "r", encoding="utf-8") as f:
+        html_content = f.read()
+    md_content = markdownify.markdownify(html_content)
+    with open(output_filename, "w") as markdown_file:
+        markdown_file.write(md_content)
 
 def process_files(directory, output_dir):
     for filename in os.listdir(directory):
@@ -9,7 +21,8 @@ def process_files(directory, output_dir):
         else:
             if not os.path.exists(output_dir):
                 os.makedirs(output_dir)
-            # 여기부터
+            convert_html_to_markdown(file_path, output_dir)
+
             
 def init():
     parser = argparse.ArgumentParser(description='')
