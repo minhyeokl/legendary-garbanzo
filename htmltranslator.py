a81bc06146a5dba7ff93444f33b0c4fbf441bdad
Minhyeok Lee <whereverigotheygo@gmail.com>
2023-05-11 12:24:35 +0900

    added htmltranslator.py

---
+++ htmltranslator.py
@@ -0,0 +1,80 @@
+import os
+import argparse
+from bs4 import BeautifulSoup
+from google.cloud import translate
+from dotenv import load_dotenv
+
+client = translate.TranslationServiceClient()
+load_dotenv(verbose=True)
+PROJECT_ID = os.getenv('PROJECT_ID')
+
+def translate_text_with_model(
+    text="",
+    project_id=PROJECT_ID,
+    location = 'us-central1',
+):
+    parent = f"projects/{project_id}/locations/{location}"
+    response = client.translate_text(
+                                parent=parent,
+                                contents=[text],
+                                mime_type='text/html',
+                                source_language_code='en',
+                                target_language_code='ko')
+
+    for translation in response.translations:
+        if translation == None:
+            continue
+        return translation.translated_text
+
+def check_html(input_file):
+    with open(input_file, "r") as f:
+        html_text = f.read()
+    soup = BeautifulSoup(html_text, "html.parser")
+    elements = soup.children
+    result = ''
+    for element in elements:
+        if element.name == None or element.name == "pre":
+            result += str(element) + '\n'
+
+        else:
+            #번역 넘기기
+            translated_par = translate_text_with_model(text=str(element))
+            result += translated_par + '\n'
+    return result
+
+def save_file(result, input_file, output_dir):
+    output_filename = os.path.basename(input_file)
+    output_filename = os.path.join(output_dir, os.path.splitext(output_filename)[0] + "_result.html")
+    with open(output_filename, "w") as r:
+        r.write(result)
+
+
+def process_files(input, output_dir):
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    result = ''
+    if os.path.isdir(input):
+        for filename in os.listdir(input):
+            file_path = os.path.join(input, filename)
+            if os.path.isdir(file_path):
+                process_files(file_path, output_dir)  # 디렉터리인 경우 재귀적으로 처리
+            else:
+                result = check_html(file_path)
+                save_file(result, file_path, output_dir)
+    else:
+        result = check_html(input)
+        save_file(result, input, output_dir)
+        
+
+
+def init():
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('source', metavar='SOURCE', type=str, help='path to the source')
+    parser.add_argument('output', metavar='OUTPUT', type=str, 
+	                default = "output/", help='path to the output ')
+    args = parser.parse_args()
+
+    process_files(args.source, args.output)
+    
+if __name__ == "__main__":
+    init()
